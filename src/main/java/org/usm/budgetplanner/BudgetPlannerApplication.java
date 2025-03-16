bd5ee0f330376f44c347025f4c43a6ca9fea2684
isimlih <iliaisakovwork@yandex.ru>
2025-03-16 12:35:12 +0200

    Initial commit

---
+++ src/main/java/org/usm/budgetplanner/BudgetPlannerApplication.java
@@ -0,0 +1,13 @@
+package org.usm.budgetplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetPlannerApplication.class, args);
+    }
+
+}
